86a4efd296ce7b5d5d42d3d928ce9d8c0676e109
Evelyn Vanielly <67793057+evelyn-vanielly@users.noreply.github.com>
2020-07-08 14:01:35 -0300

    Rename Exercicio5.java to Repetição_DoWhile_1.java

--- Repetição_DoWhile_1.java
+++ Repetição_DoWhile_1.java
@@ -10,13 +10,13 @@ public class Exercicio5 {
 		
 		do
 		{
-			System.out.println("Insira um número de 1 a 9: ");
+			System.out.println("Insira um nÃºmero de 1 a 9: ");
 			num = ler.nextInt();
 			soma= soma+num;
 			
 		}while (num!=0);
 		
-		System.out.println ("A soma dos números é: "+soma);
+		System.out.println ("A soma dos nÃºmeros Ã©: "+soma);
 	} 
 
 }
